ef6856cd0c10a05b729228dbb6628617f279f83b
Nikita <sadsnake2311@gmail.com>
2024-10-23 14:29:42 +0300

    Implemented duration of custom URL and number of usages for IP address

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -2,52 +2,76 @@ package routes
 
 import (
 	"github.com/asaskevich/govalidator"
-	_ "github.com/go-redis/redis/v8"
-	_ "github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	"os"
-	_ "strconv"
+	"strconv"
 	"net/http"
 	"url_short_v2/api/database"
 	"url_short_v2/api/helpers"
 	"github.com/gin-gonic/gin"
+	"time"
+	"fmt"
 )
 
 type request struct {
-	URL         string `json:"url"`
-	CustomShort string `json:"short"`
+	URL         	string 		`json:"url"`
+	CustomShort 	string 		`json:"short"`
 }
 
 type response struct {
-	URL         string `json:"url"`
-	CustomShort string `json:"short"`
+	URL         	string 				`json:"url"`
+	CustomShort 	string 				`json:"short"`
+	UsagesLeft		int 				`json:"left"`
+	RefreshTime 	time.Duration 		`json:"refresh"`
 }
 
 
 func ShortenURL() gin.HandlerFunc {
 	return func(c *gin.Context){
 		body := new(request)
+		resp := new(response)
+		const API_QUOTA = 10
 
 		if err := c.BindJSON(&body); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "can't parse JSON"})
 			return
 		}
+		
+		
+		r2 := database.CreateClient(1) //opening second database for IP checking
+		defer r2.Close()
 
-	//check if the input is an actual url
+		val, err := r2.Get(database.Ctx, c.ClientIP()).Result() //how many usages left
+		valInt, _ := strconv.Atoi(val)
+		fmt.Println(val)
+		limit, _ := r2.TTL(database.Ctx, c.ClientIP()).Result() //how many time is left before limit refreshing
+		if val == ""{ //if there isn't any record of this IP
+			fmt.Println("first")
+			_ = r2.Set(database.Ctx, c.ClientIP(), API_QUOTA - 1, 30*60*time.Second).Err() //putting IPislimit, how many usages are left (MAX - 1) and time left before refreshing
+			valInt = API_QUOTA
+			limit = time.Minute * 30
+		} else { // if there is
+			fmt.Println("second")
+			if valInt <= 0{ // if no usages left
+				c.JSON(http.StatusForbidden, gin.H{
+					"error": "No usages left",
+					"Time_left": limit / time.Nanosecond / time.Minute,
+				})
+				return
+			}
+		}
 
-		if !govalidator.IsURL(body.URL) {
+
+		if !govalidator.IsURL(body.URL) { //checking if the input is an actual url
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid URL"})
 			return
 		}
 
-	//check for domain err
 
 		if !helpers.RemoveDomainErr(body.URL) {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "access restricted"})
 			return
 		}
-
-	//enforce https, ssl
 
 		body.URL = helpers.EnforceHTTP(body.URL)
 		var id string
@@ -60,23 +84,26 @@ func ShortenURL() gin.HandlerFunc {
 		r := database.CreateClient(0)
 		defer r.Close()
 
-		val, err := r.Get(database.Ctx, id).Result()
+		val, _ = r.Get(database.Ctx, id).Result()
 		if val != "" {
 			c.JSON(http.StatusForbidden,gin.H{"error": "already taken"})
 			return
 		}
 		
-		err = r.Set(database.Ctx, id, body.URL, 0).Err()
+		err = r.Set(database.Ctx, id, body.URL, 30*24*60*60*time.Second).Err() //expiration date of custom url is 30 days
 		if err != nil {
 			c.JSON(http.StatusInternalServerError,gin.H{"error": err.Error()})
 			return
 		}
 
-		resp := response{
-			URL:         body.URL,
-			CustomShort: "",
-		}
+		resp.URL = body.URL
 		resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+		err = r2.Set(database.Ctx, c.ClientIP(), valInt - 1, limit).Err() //decreasing the number of usages left
+		if err != nil{
+			fmt.Println(err.Error())
+		}
+		resp.UsagesLeft = valInt - 1
+		resp.RefreshTime = limit / time.Nanosecond / time.Minute
 		c.JSON(http.StatusOK, resp)
 	}
 }
